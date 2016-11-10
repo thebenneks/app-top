f54066be441432dac7160dbd5730b1b8f6049002
Robert Bennek <thebenneks@mac.com>
2016-11-10 23:00:30 +0100

    initial commit

---
+++ src/main/java/org/thebenneks/apptop/ApptopmgrApplication.java
@@ -0,0 +1,12 @@
+package org.thebenneks.apptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptopmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptopmgrApplication.class, args);
+	}
+}
---
+++ src/test/java/org/thebenneks/apptop/ApptopmgrApplicationTests.java
@@ -0,0 +1,16 @@
+package org.thebenneks.apptop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApptopmgrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
